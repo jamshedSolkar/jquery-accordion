35a39fb934d343a11a21d51fa709bb8ccc468e91
jamshedSolkar <jamshed@sportzinteractive.net>
2020-07-02 20:32:00 +0530

    worked on new task assigned by AK

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # jquery-tuts
 
 Customise Accordion example in accordion.html
+Array sum function and box moves on mouse click example in array-mouseCords.html
--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
+  /*Accordion Starts*/
   $(".accordion__button").click(function (e) {
     e.stopPropagation();
   });
 
-  // accordion open/close with alert
   $('.accordion__head').click(function () {
     if ($(this).parent().hasClass('active')) {
       $('.accordion__box').removeClass('active');
@@ -24,4 +24,30 @@ $(document).ready(function () {
       alert('Accordion is close');
     }
   });
+  /*Accordion Ends*/
+
+  /*Array Sum Starts*/
+  function calcArray(valueArray) {
+    var totalArray = 0;
+    for (var i = 0; i < valueArray.length; i++) {
+      totalArray += Number(valueArray[i]);
+    }
+    return totalArray;
+  }
+  console.log(calcArray([05, 10, 89]));
+  /*Array Sum Ends*/
+
+  /*Mouse click cords and smooth animation start*/
+
+  // var y = event.pageY();
+  $(window).mousedown(function (event) {
+    var x = event.pageX;
+    var y = event.pageY;
+    $('.mouseCord-box').animate({
+      left: (x),
+      top: (y)
+    }, 1000);
+  });
+  /*Mouse click cords and smooth animation end*/
+
 });
